38a6cc041bfbacc48870153b55a0eb19dc1f868f
Santiago Castaño <Santiago.castanom@gmail.com>
2019-05-17 09:43:54 +1000

    Clean Tabbed Project Cryptobook

---
+++ Cryptobook/Cryptobook/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Cryptobook
+//
+//  Created by Santiago  on 5/17/19.
+//  Copyright © 2019 Santiago . All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Cryptobook/Cryptobook/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Cryptobook
+//
+//  Created by Santiago  on 5/17/19.
+//  Copyright © 2019 Santiago . All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
