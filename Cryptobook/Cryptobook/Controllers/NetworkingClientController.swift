9089a89a9ca4daf9b2f32c7e64c04665a04f41c4
Santiago Castaño <Santiago.castanom@gmail.com>
2019-05-21 23:46:49 +1000

    Add SwiftyJSON

---
+++ Cryptobook/Cryptobook/Controllers/NetworkingClientController.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkingClientController.swift
+//  Cryptobook
+//
+//  Created by Santiago  on 5/21/19.
+//  Copyright © 2019 Santiago . All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkingClient {
+    func execute(_ url: URL) {
+        <#function body#>
+    }
+}
