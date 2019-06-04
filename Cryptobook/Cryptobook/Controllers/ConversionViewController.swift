889780aee638ada6eb6f29034aac1714657b9808
Estebant97 <[esteban.t97@gmail.com]>
2019-06-04 18:15:06 +1000

    added documentation in ConversionViewController

--- Cryptobook/Cryptobook/Controllers/ConversionViewController.swift
+++ Cryptobook/Cryptobook/Controllers/ConversionViewController.swift
@@ -15,9 +15,10 @@ class ConversionViewController: UIViewController, UIPickerViewDataSource, UIPick
     @IBOutlet weak var amountTextField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
     
+    //variable for the API
     let networkingClient: NetworkingClient = NetworkingClient()
     
-    //var
+    //variables
     var currentTextField = UITextField()
     var pickerView = UIPickerView()
     var cryptoSelected : String?
@@ -39,19 +40,19 @@ class ConversionViewController: UIViewController, UIPickerViewDataSource, UIPick
         self.view.addGestureRecognizer(tapRecognizer)
         
     }
-    
+    //Deinitialization of keyboard
     deinit {
         let notificationCenter = NotificationCenter.default
         notificationCenter.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
         notificationCenter.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
     }
-    
+    //get coin prices from the API
     override func viewWillAppear(_ animated: Bool) {
         networkingClient.getCoinPrices(completion: setPrices)
     }
     
-    //fromPicker
+    //set the list of the pickerView
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -66,7 +67,7 @@ class ConversionViewController: UIViewController, UIPickerViewDataSource, UIPick
             return 0
         }
     }
-    //cryptoPicker
+    //show the options in the pickerView
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         if currentTextField == cryptoPicker {
             return assetPriceLists[row].name.capitalized
@@ -76,7 +77,7 @@ class ConversionViewController: UIViewController, UIPickerViewDataSource, UIPick
             return ""
         }
     }
-    //cryptoPicker
+    //sets the selected option and calculates the price
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         if currentTextField == cryptoPicker {
             cryptoPicker.text = assetPriceLists[row].name.capitalized
@@ -101,22 +102,22 @@ class ConversionViewController: UIViewController, UIPickerViewDataSource, UIPick
         }
         
     }
-    
+    //when it finishes editing calculates the price
     @IBAction func onEditingChanged(_ sender: Any) {
        recalculateTotal()
     }
     
-    
+    //sets the price to be calculated and recalculate value
     func setPrices(assetPriceLists: [AssetPriceList]) -> Void {
         self.assetPriceLists = assetPriceLists
         recalculateTotal()
     }
-    
+    //add done button to the textField
     func setUpTextField() {
         amountTextField.addDoneButtonToKeyboard()
     }
     
-    
+    //manages amount textField
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         // User pressed the delete-key to remove a character, this is always valid, return true to allow change
         
@@ -132,14 +133,14 @@ class ConversionViewController: UIViewController, UIPickerViewDataSource, UIPick
         }
         return false
     }
-    
+    //add event listeners to the textFields
     func setupTextFieldListeners() {
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillShowNotification, object: nil)
     }
-    
+    //adjust the keyboard in every device
     @objc func adjustForKeyboard(notification: Notification) {
         guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
             return
@@ -152,12 +153,12 @@ class ConversionViewController: UIViewController, UIPickerViewDataSource, UIPick
             }
         }
     }
-    
+    //hides keyboard in the amount textField
     @objc
     func hideKeyboard() {
         amountTextField.resignFirstResponder()
     }
-    
+    //recalculates value depending on selections
     func recalculateTotal() {
         var amount : Double {
             if amountTextField.text!.isEmpty {
